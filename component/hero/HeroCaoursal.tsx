2996531305912cc21091590dee7c955f9249298d
pavanleton <pavankumarkpkp@gmail.com>
2021-11-12 18:11:17 +0530

    fix carousal overflow

--- component/hero/HeroCaoursal.tsx
+++ component/hero/HeroCaoursal.tsx
@@ -18,14 +18,17 @@ const HeroCaoursal: React.FC = () => {
 
     };
     return <>
-        <Slider
-            {...settings}
-        >
-            {[1, 2, 3].map(() => <Box w={"100%"} h={{base: "100%", md:"350px", lg:"100vh"}} position={"relative"}>
-                <Box zIndex={30} pos={"absolute"} width={"100%"} height={"100%"} background={"black"} opacity={"0.4"}/>
-                <Image src={"https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"} height={"100%"} w={"100%"} />
-            </Box>)}
-        </Slider>
+        <Box height={{base: "100%", md:"400px", lg:"100vh"}} width={"100%"} overflow={"hidden"}>
+            <Slider
+                {...settings}
+
+            >
+                {[1, 2, 3].map(() => <Box w={"100%"} h={"100%"} position={"relative"}>
+                    <Box zIndex={30} pos={"absolute"} width={"100%"} height={"100%"} background={"black"} opacity={"0.4"}/>
+                    <Image src={"https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"} height={"100%"} w={"100%"} objectFit={"cover"} objectPosition={"center"} />
+                </Box>)}
+            </Slider>
+        </Box>
     </>
 }
 
--- component/navbar/index.tsx
+++ component/navbar/index.tsx
@@ -1,17 +1,17 @@
 import {NextPage} from "next";
-import {FaPhoneAlt} from "react-icons/fa";
-import {AiFillFacebook, AiFillInstagram} from "react-icons/ai";
-import {IoLogoYoutube} from "react-icons/io";
 import {FiMenu} from "react-icons/fi";
 import {
     Box, Flex, Text, Image,
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuItemOption,
+    MenuGroup,
+    MenuOptionGroup,
+    MenuIcon,
+    MenuCommand,
+    MenuDivider,
     useDisclosure, Button, IconButton
 } from "@chakra-ui/react";
 import React from "react";
@@ -39,20 +39,6 @@ const BaseNavbar = () => {
 const Index: NextPage = () => {
 
     return <>
-        {/*<Flex flexDirection={"row-reverse"} bg={"brandPrimary.300"} color={"white"} px={"1rem"} py={".5rem"} alignItems={"center"} justify={"space-between"} >*/}
-        {/*    <Flex alignItems={"center"}  gridGap={".3rem"} fontSize={"3xl"}>*/}
-        {/*        <FaPhoneAlt/>*/}
-        {/*        <Flex flexDirection={"column"}>*/}
-        {/*            <a href={"tel:+919000900009"}><Text fontSize={"md"}>+91-9000900009</Text></a>*/}
-        {/*            <a href={"tel:+919000900009"}><Text fontSize={"md"}>+91-9000900009</Text></a>*/}
-        {/*        </Flex>*/}
-        {/*    </Flex>*/}
-        {/*    <Flex gridGap={"1rem"} fontSize={"2xl"}>*/}
-        {/*        <AiFillFacebook/>*/}
-        {/*        <AiFillInstagram/>*/}
-        {/*        <IoLogoYoutube/>*/}
-        {/*    </Flex>*/}
-        {/*</Flex>*/}
         <Flex zIndex={50} position={{base:"relative", lg: "absolute"}} w={"100%"} bg={{base: "brandPrimary.400", lg:"transparent"}} alignItems={"center"} justify={"space-between"} p={"1rem"}>
             <Box w={["50px", "80px"]} h={["50px", "80px"]}>
                 <Image src={"https://i.ibb.co/RNF7gR7/LOGO.png"} width={"100%"} height={"100%"} filter={"brightness(0) invert(1)"}/>
@@ -67,9 +53,16 @@ const Index: NextPage = () => {
                     </Text>
                 </Box>
                 <Box transition={"all .3s linear"} _hover={{bg:"brandPrimary.50", color:"brandPrimary.600"}} cursor={"pointer"} borderRadius={"6px"} px={"1rem"} py={".5rem"}>
-                    <Text fontSize={"lg"}>
-                        Our Programs
-                    </Text>
+                    <Menu>
+                        <MenuButton as={Text}>
+                            Actions
+                        </MenuButton>
+                        <MenuList>
+                            <MenuItem  color={"heading"}>Program 1</MenuItem>
+                            <MenuItem  color={"heading"}>Program 2</MenuItem>
+                            <MenuItem  color={"heading"}>Program 3</MenuItem>
+                        </MenuList>
+                    </Menu>
                 </Box>
                 <Box transition={"all .3s linear"} _hover={{bg:"brandPrimary.50", color:"brandPrimary.600"}} cursor={"pointer"} borderRadius={"6px"} px={"1rem"} py={".5rem"}>
                     <Text fontSize={"lg"}>
